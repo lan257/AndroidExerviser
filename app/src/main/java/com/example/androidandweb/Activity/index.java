4a6c73def14e99cfbc81a308c4cf0c1532ae2681
lang <zut177@hotmail.com>
2024-01-13 13:38:51 +0800

    app增加主页和注册两个无逻辑页面，之间实现跳转，

---
+++ app/src/main/java/com/example/androidandweb/Activity/index.java
@@ -0,0 +1,16 @@
+package com.example.androidandweb.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidandweb.R;
+
+public class index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
