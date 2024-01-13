197475d6901ad6602ff420d48b6ce67ed6e1c1d2
lang <zut177@hotmail.com>
2024-01-13 11:50:50 +0800

    app添加记住密码，自动登录，自动勾选同意，判断勾选，存储jwt令牌，点击清空订阅框

--- app/src/main/java/com/example/androidandweb/MainActivity.java
+++ app/src/main/java/com/example/androidandweb/MainActivity.java
@@ -1,68 +1,145 @@
 package com.example.androidandweb;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidandweb.O_solidObjects.simpleObjects.Result;
 import com.example.androidandweb.O_solidObjects.simpleObjects.privateUser;
 import com.example.androidandweb.http.postNoJwt;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    // 获取SharedPreferences对象，参数为文件名和访问模式
+    SharedPreferences sharedPreferences = MyApp.getSharedPreferences();
+    SharedPreferences.Editor editor = sharedPreferences.edit();
     postNoJwt postNoJwt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button AAWLogin=findViewById(R.id.AAWLogin);
-        AAWLogin.setOnClickListener(this);
-        Button updateVid=findViewById(R.id.updateVid);
-        updateVid.setOnClickListener(this);
+        Button AAWLogin=findViewById(R.id.AAWLogin);//主动登录
+        AAWLogin.setOnClickListener(this);//设置事件
+        Button updateVid=findViewById(R.id.updateVid);//更新订阅
+        updateVid.setOnClickListener(this);//设置事件
+        TextView Vid=findViewById(R.id.textVid);
+        Vid.setOnClickListener(this);
+        loginAuto();
     }
+//按钮事件判定
     public void onClick(View v){
         if(v.getId()==R.id.AAWLogin){
-            TextView email=findViewById(R.id.editTextEmailAddress);
-            TextView password=findViewById(R.id.editTextPassword);
-            String PUEmail=email.getText().toString();
-            String PUPassword=password.getText().toString();
-            privateUser pu=new privateUser(PUEmail,PUPassword);
-            //发送http请求，需要：url,数据，其中数据包含Private{email,password}
-            String url = "/login";
-
-            // 调用方法发送POST请求
-            postNoJwt.sendPostRequest(url, pu, new postNoJwt.OnResultListener() {
-                public void onResult(Result result) {
-                    // 处理返回的Result对象
-                    if (result != null) {
-
-
-
-                        //数据处理
-                        Toast.makeText(MainActivity.this, result.getMsg(), Toast.LENGTH_SHORT).show();
-                        // 还可以通过result.getData()获取数据
-                    } else {
-                        Toast.makeText(MainActivity.this, "Error in HTTP POST request", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });}
+            login();//调用登录方法
+        }
         if(v.getId()==R.id.updateVid){
-
             TextView Vid=findViewById(R.id.textVid);
             String vid=Vid.getText().toString();
-            // 获取SharedPreferences对象，参数为文件名和访问模式
-            SharedPreferences sharedPreferences = MyApp.getSharedPreferences();
-            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("vid", vid);
             editor.apply();
             Toast.makeText(MainActivity.this, "更新成功", Toast.LENGTH_SHORT).show();
         }
+        if(v.getId()==R.id.textVid){
+            TextView Vid=findViewById(R.id.textVid);
+        Vid.setText("");
+        }
+    }
+
+
+    //登录方法
+    void login(){
+        CheckBox allow=findViewById(R.id.allowAll);
+        if(allow.isChecked()){
+        TextView email=findViewById(R.id.editTextEmailAddress);
+        TextView password=findViewById(R.id.editTextPassword);
+        String PUEmail=email.getText().toString();
+        String PUPassword=password.getText().toString();
+        privateUser pu=new privateUser(PUEmail,PUPassword);
+        //发送http请求，需要：url,数据，其中数据包含Private{email,password}
+        String url = "/login";
+
+        // 调用方法发送POST请求
+        postNoJwt.sendPostRequest(url, pu, new postNoJwt.OnResultListener() {
+            public void onResult(Result result) {
+                // 处理返回的Result对象
+                if (result != null) {
+
+                    //数据处理
+//                    Toast.makeText(MainActivity.this, result.msg, Toast.LENGTH_SHORT).show();
+                    if(result.iu!=0){//登陆成功
+                    //将jwt存入键值对存储
+                    editor.putString("token", result.data.toString());
+                    editor.apply();
+//                    String jwt=sharedPreferences.getString("token", "none");//读取token
+                    Toast.makeText(MainActivity.this, "登陆成功", Toast.LENGTH_LONG).show();
+
+                    CheckBox remember=findViewById(R.id.ARemember);//检测记住账号
+                    CheckBox auto=findViewById(R.id.AAuto);//检测自动登录
+
+                    //存储登录信息
+                        if(remember.isChecked()){
+                            editor.putString("remember","true");
+                            editor.putString("email",PUEmail);
+                            editor.putString("password",PUPassword);
+                            editor.apply();
+                            if (auto.isChecked()){
+                                editor.putString("auto","true");
+                                editor.apply();
+                            }else {editor.putString("auto","false");
+                            editor.apply();}
+                        }else {
+                            editor.putString("remember","false");
+                            editor.apply();
+                        }
+
+                        //跳转主页，待做
+                    }
+
+                    // 还可以通过result.getData()获取数据
+                } else {
+                    Toast.makeText(MainActivity.this, "服务器连接失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });}else {
+            Toast.makeText(MainActivity.this, "你需要勾选同意app的相关规定", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //读取登录信息
+    void loginAuto() {
+        CheckBox remember=findViewById(R.id.ARemember);//检测记住账号
+        CheckBox auto=findViewById(R.id.AAuto);//检测自动登录
+        TextView email=findViewById(R.id.editTextEmailAddress);
+        TextView password=findViewById(R.id.editTextPassword);
+        if(!sharedPreferences.getString("remember", "").equals("")){
+        String REM=sharedPreferences.getString("remember", "");
+        if(REM.equals("true")){
+            email.setText(sharedPreferences.getString("email", ""));
+            password.setText(sharedPreferences.getString("password", ""));
+            remember.setChecked(true);
+            if (!sharedPreferences.getString("auto", "").equals("")){
+            String AUTO=sharedPreferences.getString("auto", "");
+            if(AUTO.equals("true")){
+                auto.setChecked(true);
+//                Thread.sleep(5000);
+                    // 在触发登录前等待5秒
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(auto.isChecked()){
+                                login();
+                            }
+                        }
+                    }, 5000);
+                }
+            }
+        }}
     }
 }
--- app/src/main/java/com/example/androidandweb/O_solidObjects/simpleObjects/Result.java
+++ app/src/main/java/com/example/androidandweb/O_solidObjects/simpleObjects/Result.java
@@ -8,35 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor//无参构造函数
 @AllArgsConstructor//全参构造函数
 public class Result {
-    private int iu;//如果数据正确且没有任何问题，iu=1,有任何问题iu=0
-    private String msg;//提示信息，比如登录功能，msg=“登陆成功”，
+    public int iu;//如果数据正确且没有任何问题，iu=1,有任何问题iu=0
+    public String msg;//提示信息，比如登录功能，msg=“登陆成功”，
     // 因为密码登陆失败，msg="账号或密码错误",比如返回的数据是用户email.msg="这是用户email"
-    private Object data;//返回的数据，比如返回用户全部信息，可以把用户信息封装成一个实体user，
+    public Object data;//返回的数据，比如返回用户全部信息，可以把用户信息封装成一个实体user，
     // data=new user;同理可以等于一个基本数据类型，一个列表，一个实体，一个文件等等。出现问题视情况赋值
 
-    public int getIu() {
-        return iu;
-    }
-
-
-
-    public void setIu(int iu) {
-        this.iu = iu;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
