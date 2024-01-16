82210eef606b7b290146c802f68e2e2837059161
lang <zut177@hotmail.com>
2024-01-16 23:43:52 +0800

    左隐藏页菜单点击时间创建，在线图片，视频展示

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.androidandweb"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,6 +36,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.google.android.exoplayer:exoplayer-core:2.19.1")
+    implementation ("com.google.android.exoplayer:exoplayer-ui:2.19.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/androidandweb/Activity/index.java
+++ app/src/main/java/com/example/androidandweb/Activity/index.java
@@ -1,16 +1,114 @@
 package com.example.androidandweb.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.androidandweb.R;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.material.navigation.NavigationView;
 
-public class index extends AppCompatActivity {
+import lombok.NonNull;
+
+public class index extends AppCompatActivity implements View.OnClickListener {
+    private DrawerLayout index;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        index=findViewById(R.id.index);
+        ImageView mi=findViewById(R.id.userImg);
+        mi.setOnClickListener(this);
+        NavigationView navigationView = findViewById(R.id.left);
+        ImageView imageView = findViewById(R.id.userImg);
+        String imageUrl = "https://dmimg.5054399.com/allimg/2012nianli/qianxingba/24.jpg";
+
+
+// 使用 Glide 或其他图片加载库加载在线图片
+        Glide.with(this).load(imageUrl).into(imageView);
+
+
+        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
+        PlayerView playerView = findViewById(R.id.playerView);
+
+// 将Player关联到PlayerView
+        playerView.setPlayer(player);
+
+// 准备媒体资源
+        MediaItem mediaItem = MediaItem.fromUri("https://sns-video-bd.xhscdn.com/spectrum/06f77ba84733159ec47e3c525fd7ca0f0eabdfb4");
+        player.setMediaItem(mediaItem);
+
+// 准备播放器
+        player.prepare();
+
+// 开始播放
+        player.play();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // 在这里处理菜单项的点击事件
+            if(item.getItemId()==R.id.select1){
+        Log.i("","我点击了选项一");
+        Toast.makeText(index.this, "选项一", Toast.LENGTH_SHORT).show();
     }
+
+    if(item.getItemId()==R.id.select2){Log.i("","我点击了选项二");
+        Toast.makeText(index.this, "选项二", Toast.LENGTH_SHORT).show();
+    }if(item.getItemId()==R.id.select3){
+        Toast.makeText(index.this, "选项三", Toast.LENGTH_SHORT).show();
+    }if(item.getItemId()==R.id.select4){
+        Toast.makeText(index.this, "选项四", Toast.LENGTH_SHORT).show();
+    }
+    return true;
+        });
+
+    }
+    public void onClick(View v) {
+        if(v.getId()==R.id.userImg){
+            index.openDrawer(GravityCompat.START);
+        }
+
+    }
+
+//
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.nav_menu,menu);
+//        return true;
+//    }
+
+
+//    public boolean onOptionsItemSelected(MenuItem item){
+//
+//
+//Log.i("","点击按钮让我看见");
+//        Log.i("","我点击了"+item.getItemId());
+//        if(item.getItemId()==R.id.select1){
+//            Log.i("","我点击了选项一");
+//            Toast.makeText(this, "选项一", Toast.LENGTH_SHORT).show();
+//        }
+//
+//        if(item.getItemId()==R.id.select2){Log.i("","我点击了选项二");
+//            Toast.makeText(this, "选项二", Toast.LENGTH_SHORT).show();
+//        }if(item.getItemId()==R.id.select3){
+//            Toast.makeText(this, "选项三", Toast.LENGTH_SHORT).show();
+//        }if(item.getItemId()==R.id.select4){
+//            Toast.makeText(this, "选项四", Toast.LENGTH_SHORT).show();
+//        }
+//        return true;
+//    }
+
 }
